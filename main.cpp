47916b6c3914ec1d5873c5bebc16c58451f1c90d
nmcarp99 <nmcarp99@gmail.com>
2020-05-18 18:28:50 -0400

    added font menu. still need to add exit and joystick

--- main.cpp
+++ main.cpp
@@ -1,29 +1,139 @@
-#include "draw.h"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <iostream>
+
+enum menuOption {
+	menu,
+	game,
+	customizeCharacter,
+	fontSelector
+};
+
+struct selectFont {
+	ALLEGRO_FONT* Ancient_Medium;
+	ALLEGRO_FONT* Barbarian;
+	ALLEGRO_FONT* GODOFWAR;
+	ALLEGRO_FONT* Kingthings_Spike;
+} fonts;
 
 ALLEGRO_DISPLAY* display = NULL;
 ALLEGRO_EVENT_QUEUE* event_queue = NULL;
 ALLEGRO_TIMER* timer = NULL;
+ALLEGRO_FONT* selectedFont = NULL;
+
+ALLEGRO_BITMAP* stars = NULL;
 
 bool f11 = false;
 
+int selectedMenuOption = menuOption::fontSelector;
+
 const float FPS = 1.0 / 60;
+
+int checkFontButtons(int mouseX, int mouseY) {
+	// kingsthing spike
+	if (mouseX >= 25 && mouseX <= 1055 && mouseY >= 110 && mouseY <= 160) {
+		selectedFont = fonts.Kingthings_Spike;
+	}
+
+	// Barbarian
+	if (mouseX >= 25 && mouseX <= 1055 && mouseY >= 195 && mouseY <= 245) {
+		selectedFont = fonts.Barbarian;
+	}
+
+	// GODOFWAR
+	if (mouseX >= 25 && mouseX <= 1055 && mouseY >= 280 && mouseY <= 330) {
+		selectedFont = fonts.GODOFWAR;
+	}
+
+	return 0;
+}
+
+int draw(int option)
+{
+	int mouseX, mouseY, xPos, yPos;
+	al_get_mouse_cursor_position(&mouseX, &mouseY);
+	al_get_window_position(display, &xPos, &yPos);
+	mouseX = mouseX - xPos;
+	mouseY = mouseY - yPos - 30;
+	switch (option) {
+	case menuOption::customizeCharacter:
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_draw_filled_rounded_rectangle(0, 0, 1080, 50, 50, 25, al_map_rgb(255, 255, 255));
+		al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 10, ALLEGRO_ALIGN_CENTER, "Character Customizer");
+		break;
+	case menuOption::fontSelector:
+		al_draw_bitmap(stars, 0, 0, 0);
+		al_draw_filled_rounded_rectangle(0, 10, 1080, 60, 50, 25, al_map_rgb(255, 255, 255));
+		al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 20, ALLEGRO_ALIGN_CENTER, "Font Selecter");
+
+		// kingsthing spike
+		if (mouseX >= 25 && mouseX <= 1055 && mouseY >= 110 && mouseY <= 160) {
+			al_draw_filled_rounded_rectangle(25, 110, 1055, 160, 50, 25, al_map_rgb(150, 150, 150));
+			al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 120, ALLEGRO_ALIGN_CENTER, "Kingsthing Spike");
+		}
+		else {
+			al_draw_filled_rounded_rectangle(25, 110, 1055, 160, 50, 25, al_map_rgb(255, 255, 255));
+			al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 120, ALLEGRO_ALIGN_CENTER, "Kingsthing Spike");
+		}
+
+		// Barbarian
+		if (mouseX >= 25 && mouseX <= 1055 && mouseY >= 195 && mouseY <= 245) {
+			al_draw_filled_rounded_rectangle(25, 195, 1055, 245, 50, 25, al_map_rgb(150, 150, 150));
+			al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 205, ALLEGRO_ALIGN_CENTER, "Barbarian");
+		}
+		else {
+			al_draw_filled_rounded_rectangle(25, 195, 1055, 245, 50, 25, al_map_rgb(255, 255, 255));
+			al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 205, ALLEGRO_ALIGN_CENTER, "Barbarian");
+		}
+
+		if (mouseX >= 25 && mouseX <= 1055 && mouseY >= 280 && mouseY <= 330) {
+			al_draw_filled_rounded_rectangle(25, 280, 1055, 330, 50, 25, al_map_rgb(150, 150, 150));
+			al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 290, ALLEGRO_ALIGN_CENTER, "GODOFWAR");
+		}
+		else {
+			al_draw_filled_rounded_rectangle(25, 280, 1055, 330, 50, 25, al_map_rgb(255, 255, 255));
+			al_draw_text(selectedFont, al_map_rgb(255, 0, 0), 540, 290, ALLEGRO_ALIGN_CENTER, "GODOFWAR");
+		}
+		break;
+	}
+	al_flip_display();
+
+	return 0;
+}
 
 int main()
 {
 	// initialize allegro
 	al_init();
+	al_install_mouse();
 	al_init_native_dialog_addon();
 	al_install_keyboard();
 	al_init_primitives_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_init_image_addon();
 
 	// create event queue
 	event_queue = al_create_event_queue();
 
 	// create new display
-	al_set_new_display_flags(ALLEGRO_WINDOWED | ALLEGRO_RESIZABLE);
 	al_set_new_window_position(200, 100);
 	al_set_new_window_title("Space Game");
 	display = al_create_display(1080, 640);
+
+	// load images
+	stars = al_load_bitmap("stars.jpg");
+
+	// load fonts
+	fonts.Ancient_Medium = al_load_font("Ancient Medium.ttf", 25, NULL);
+	fonts.Barbarian = al_load_font("Barbarian.ttf", 25, NULL);
+	fonts.GODOFWAR = al_load_font("GODOFWAR.ttf", 25, NULL);
+	fonts.Kingthings_Spike = al_load_font("Kingthings Spike.ttf", 25, NULL);
+	selectedFont = fonts.GODOFWAR;
 
 	// create timer
 	timer = al_create_timer(FPS);
@@ -32,6 +142,7 @@ int main()
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_mouse_event_source());
 
 	// start the timer
 	al_start_timer(timer);
@@ -50,18 +161,13 @@ int main()
 			running = false;
 			break;
 		case ALLEGRO_EVENT_TIMER:
-			draw(menuOption::customizeCharacter);
+			draw(selectedMenuOption);
 			break;
-		case ALLEGRO_EVENT_KEY_UP:
-			switch (event.keyboard.keycode) {
-			case ALLEGRO_KEY_F11:
-				al_toggle_display_flag(display, ALLEGRO_FULLSCREEN_WINDOW, !f11);
-				f11 = !f11;
-				break;
-			case ALLEGRO_KEY_ESCAPE:
-				al_toggle_display_flag(display, ALLEGRO_FULLSCREEN_WINDOW, false);
-				break;
+		case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+			if (selectedMenuOption == menuOption::fontSelector) {
+				checkFontButtons(event.mouse.x, event.mouse.y);
 			}
+			break;
 		}
 	}
 }
